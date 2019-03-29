128ef01f6dd14864b518380850d9fcb303545ea3
kawoka <br2jn51@yahoo.co.jp>
2019-03-29 21:53:12 +0900

    add spec

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import path from 'path'
 import process from 'process'
 import {makeDirObj} from './lib/makeDirObj'
 import {makeDiagram} from './lib/makeDiagram'
+
 const rootDir = path.resolve( process.argv[2] || process.cwd() ) //引数が無いときはカレントディレクトリを対象とする
 const main = async () => {
   const dirObj = await makeDirObj(rootDir).catch((err) => {
-    console.error(err)
+    throw err
   })
-  // console.log(JSON.stringify(dirObj))
   const diagram = makeDiagram(dirObj)
   console.log(diagram)
 }
--- test/index.spec.js
+++
@@ -1,7 +0,0 @@
-import {test} from '../lib/index'
-import expect from 'expect.js'
-describe('test', () => {
-  it('return string', () => {
-    expect(test()).to.eql('this is function')
-  })
-})
